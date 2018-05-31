9091c5298fd70bbcccc8bb0aa85c24b28384dd60
andhikayuana <andhikayuana@gmail.com>
2018-05-31 23:29:20 +0700

    script update .env

--- andekata-scripts/andekata-client-setup.bat
+++ andekata-scripts/andekata-client-setup.bat
@@ -3,17 +3,6 @@ ECHO OFF
 CLS
 ECHO -by Ajaro 2018
 
-REM --- check requirements... ---
-REM ECHO check internet connection
-REM PING www.google.nl -n 1 -w 1000
-REM IF ERRORLEVEL 1 EXIT /b ERRORLEVEL
-REM ECHO check git 
-REM git --version
-REM ECHO check node
-REM node --version
-REM ECHO check npm
-REM CALL npm --version
-
 REM --- clone andekata-client to local
 ECHO clone andekata-client to local
 git clone https://github.com/ajaroid/andekata-client.git
